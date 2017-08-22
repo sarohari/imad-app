74a3c0bbfc29f316a33597aed4344c037c4a8d64
sarohari <saro_hari@yahoo.com>
2017-08-22 22:50:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ function CreateTemplate(data) {
     <html>
     <head>
         <title>${title}</title>
+        <link href="ui/style.css" rel="stylesheet">
     </head>
     <body>
         <H1>${content}</H1>
