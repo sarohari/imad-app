3236471e70684e89facc688c3ca47259ce24285d
sarohari <saro_hari@yahoo.com>
2017-08-23 21:51:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "hi to all";
+
+var img = document.getElementById("dino");
+
+img.onclick = function () {
+    img.style.marginLeft = 100px;
+};
+
+
