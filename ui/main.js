a728b3649f7f3f88851b2f76b736907837a443da
sarohari <saro_hari@yahoo.com>
2017-08-23 21:57:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,12 +9,12 @@ var img = document.getElementById("dino");
 var marginleft = 100;
 
 function moveRight(){
-marginleft = marginleft + 10 ;
+marginleft = marginleft + 5 ;
 img.style.marginLeft = marginleft + 'px';
 };
 
 img.onclick = function () {
-var interval = setInterval(moveRight,100);
+var interval = setInterval(moveRight,50);
 };
 
 
